c03156678160fff7caf1371f96e0b13aeda64f53
wangdongsheng <wangdongsheng@100tal.com>
2019-09-09 19:35:38 +0800

    提交

--- src/config/router.js
+++ src/config/router.js
@@ -1,167 +1,166 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
-Vue.use(Router);
+Vue.use (Router);
 
-const routes = [{
+const routes = [
+  {
     path: '*',
-    redirect: '/home'
+    redirect: '/home',
   },
   {
     name: 'home',
-    component: () => import('../pages/home/index'),
+    component: () => import ('../pages/home/index'),
     meta: {
-      title: '首页'
-    }
+      title: '首页',
+    },
   },
   {
     path: '/user',
-    component: () => import('../pages/user/index'),
+    component: () => import ('../pages/user/index'),
     name: 'user',
     meta: {
-      title: '我的'
-    }
+      title: '我的',
+    },
   },
   {
     path: '/user/order',
-    component: () => import('../pages/user/order/list'),
+    component: () => import ('../pages/user/order/list'),
     meta: {
-      title: '我的订单'
-    }
+      title: '我的订单',
+    },
   },
   {
     path: '/user/order/:id',
-    component: () => import('../pages/user/order/info'),
+    component: () => import ('../pages/user/order/info'),
     meta: {
-      title: '订单详情'
-    }
+      title: '订单详情',
+    },
   },
   {
     path: '/user/profit',
-    component: () => import('../pages/user/profit/list'),
+    component: () => import ('../pages/user/profit/list'),
     meta: {
-      title: '提现记录'
-    }
+      title: '提现记录',
+    },
   },
   {
     path: '/user/profit/detail',
-    component: () => import('../pages/user/profit/detail'),
+    component: () => import ('../pages/user/profit/detail'),
     meta: {
-      title: '收益详情'
-    }
+      title: '收益详情',
+    },
   },
   {
     path: '/user/withdrawal',
-    component: () => import('../pages/user/profit/withdrawal'),
+    component: () => import ('../pages/user/profit/withdrawal'),
     meta: {
-      title: '提现'
-    }
+      title: '提现',
+    },
   },
   {
     path: '/user/share',
-    component: () => import('../pages/user/share/list'),
+    component: () => import ('../pages/user/share/list'),
     meta: {
-      title: '推广记录'
-    }
+      title: '推广记录',
+    },
   },
   {
     path: '/city',
-    component: () => import('../pages/cities/index'),
+    component: () => import ('../pages/cities/index'),
     meta: {
-      title: '城市'
-    }
+      title: '城市',
+    },
   },
   {
     path: '/search',
-    component: () => import('../pages/search/index'),
+    component: () => import ('../pages/search/index'),
     meta: {
-      title: '搜素'
-    }
+      title: '搜索',
+    },
   },
   {
     path: '/list',
-    component: () => import('../pages/product/list'),
+    component: () => import ('../pages/product/list'),
     meta: {
-      title: 'listtest'
-    }
+      title: 'listtest',
+    },
   },
   // 详情
   {
     path: '/goods/:id',
-    component: () => import('../pages/product/goods/detail'),
+    component: () => import ('../pages/product/goods/detail'),
     meta: {
-      title: '商品详情'
-    }
+      title: '商品详情',
+    },
   },
   {
     path: '/goods/order/:id',
-    component: () => import('../pages/product/goods/order'),
+    component: () => import ('../pages/product/goods/order'),
     meta: {
-      title: '商品提交预定'
-    }
+      title: '商品提交预定',
+    },
   },
   {
     path: '/hotel/:id',
-    component: () => import('../pages/product/hotel/detail'),
+    component: () => import ('../pages/product/hotel/detail'),
     meta: {
-      title: '酒店详情'
-    }
+      title: '酒店详情',
+    },
   },
   {
     name: 'name',
     path: '/hotel/room/:id',
-    component: () => import('../pages/product/hotel/room'),
+    component: () => import ('../pages/product/hotel/room'),
     meta: {
-      title: '房型介绍'
-    }
+      title: '房型介绍',
+    },
   },
   {
     path: '/hotel/order/:id',
-    component: () => import('../pages/product/hotel/order'),
+    component: () => import ('../pages/product/hotel/order'),
     meta: {
-      title: '酒店提交预定'
-    }
+      title: '酒店提交预定',
+    },
   },
   {
     path: '/scenic/:id',
-    component: () => import('../pages/product/scenic/detail'),
+    component: () => import ('../pages/product/scenic/detail'),
     meta: {
-      title: '景区详情'
-    }
+      title: '景区详情',
+    },
   },
   {
     path: '/scenic/order/:id',
-    component: () => import('../pages/product/scenic/order'),
+    component: () => import ('../pages/product/scenic/order'),
     meta: {
-      title: '景区提交预定'
-    }
+      title: '景区提交预定',
+    },
   },
   {
     path: '/detail/:id',
-    component: () => import('../pages/detail/test'),
+    component: () => import ('../pages/detail/test'),
     meta: {
-      title: '测试商品详情'
-    }
+      title: '测试商品详情',
+    },
   },
 ];
 
 // add route path
-routes.forEach(route => {
+routes.forEach (route => {
   route.path = route.path || '/' + (route.name || '');
 });
 
-const router = new Router({
-  routes
+const router = new Router ({
+  routes,
 });
 
-router.beforeEach((to, from, next) => {
+router.beforeEach ((to, from, next) => {
   const title = to.meta && to.meta.title;
   if (title) {
     document.title = title;
   }
-  next();
+  next ();
 });
 
-export {
-  router
-};
+export {router};
